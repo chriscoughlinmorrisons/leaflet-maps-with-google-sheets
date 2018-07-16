01bd331be843c5ffeb31c07bcffc7bc60f8f0d36
chriscoughlinmorrisons <41282805+chriscoughlinmorrisons@users.noreply.github.com>
2018-07-16 08:23:45 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1CLer78hNsy9Zk8G9ssM-Cd5DwYeJCAouLRJWvpmzadg/edit#gid=164271551';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1CLer78hNsy9Zk8G9ssM-Cd5DwYeJCAouLRJWvpmzadg/edit#gid=0';
